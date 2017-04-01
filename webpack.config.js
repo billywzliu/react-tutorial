c19d9c9d3ecef12ca68d1d74ca81a8daebd584aa
billywzliu <billywzliu@outlook.com>
2017-03-31 22:58:00 -0700

    Add basic features

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,31 @@
-var debug = process.env.NODE_ENV !== "production";
-var webpack = require('webpack');
+let debug = process.env.NODE_ENV !== 'production';
+let webpack = require('webpack');
+let path = require('path');
 
 module.exports = {
-  context: __dirname,
-  // devtool: debug ? "inline-sourcemap" : null,
-  entry: "./js/scripts.js",
+  context: path.join(__dirname, 'src'),
+  // devtool: debug ? 'inline-sourcemap' : null,
+  entry: './js/client.js',
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015', 'stage-0'],
+          plugins: ['react-html-attrs', 'transform-decorators-legacy', 'transform-class-properties'],
+        },
+      },
+    ],
+  },
   output: {
-    path: __dirname + "/js",
-    filename: "scripts.min.js"
+    path: __dirname + '/src/',
+    filename: 'client.min.js',
   },
   plugins: debug ? [] : [
+    // new webpack.optimize.DedupePlugin(),
     new webpack.optimize.OccurrenceOrderPlugin(),
-    new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
+    new webpack.optimize.UglifyJsPlugin({mangle: false, sourcemap: false}),
   ],
 };
